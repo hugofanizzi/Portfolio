9d1adfaafa6dd6369c3fb21511c7587e138b0cf7
Hugo <hj.fanizzi@gmail.com>
2022-08-25 16:16:42 +0200

    fix information json

--- pages/api/information.js
+++ pages/api/information.js
@@ -1,14 +1,15 @@
 const information = [
-  (aboutMeInformation = {
-    name: `About Me`,
+  {
+    aboutMeInformation: {
+      name: `About Me`,
 
-    title: "Hugo José Esparza Fanizzi",
+      title: "Hugo José Esparza Fanizzi",
 
-    subtitle: `No creo en las palabras "No se puede" o "No se hacerlo" para todo hay solución y todos tenemos la capacidad de aprender lo que queramos`,
+      subtitle: `No creo en las palabras "No se puede" o "No se hacerlo" para todo hay solución y todos tenemos la capacidad de aprender lo que queramos`,
 
-    summary: `¡Que bueno que has llegado hasta aquí!. Soy Hugo José Esparza Fanizzi, nacido en Venezuela, lleno de aspiraciones y metas por cumplir que seguro conseguire aqui en España. No creo en la palabra "No se puede" o  "No se hacerlo", pienso que ahora en el mundo que vivimos hay soluciones para todo y por eso me he querido sumergirme en la programación ya que es un mar de infinitas posibilidades. Me gusta mucho aprender cosas nuevas y me esmero por conseguir los objetivos que me propongo sin importar las veces que caiga seran mas las veces que me levantaré.`,
+      summary: `¡Que bueno que has llegado hasta aquí!. Soy Hugo José Esparza Fanizzi, nacido en Venezuela, lleno de aspiraciones y metas por cumplir que seguro conseguire aqui en España. No creo en la palabra "No se puede" o  "No se hacerlo", pienso que ahora en el mundo que vivimos hay soluciones para todo y por eso me he querido sumergirme en la programación ya que es un mar de infinitas posibilidades. Me gusta mucho aprender cosas nuevas y me esmero por conseguir los objetivos que me propongo sin importar las veces que caiga seran mas las veces que me levantaré.`,
 
-    story: `Trabajo desde los 15 años, siempre he sido un poco inquieto por salir adelante, termine el bachillerato y empece a estudiar Ingenieria Eléctronica, la cual no pude terminar por iniciar mi nueva vida en España, llegue a españa el 12 de Octubre del 2017 junto a mi esposa, llenos de iluciones y ganas de salir adelante, a los tres meses consegui mi primer trabajo como dependiente de tienda en la cual me ofrecieron la posibilidad de aprender a reparar equipos eléctronicos, sin dudar acepte y aprendi todo lo que me enseñaron y con el tiempo reforce esos conocimientos y me especialice mas en el sector, sin embargo, no me llenaba del todo. Siempre me ha gustado realizar formatos en excel y hacer fórmulas, entonces por que no estudiar algo en lo que podre hacer de todo, por tal motivo decidi estudiar desarrollo web.
+      story: `Trabajo desde los 15 años, siempre he sido un poco inquieto por salir adelante, termine el bachillerato y empece a estudiar Ingenieria Eléctronica, la cual no pude terminar por iniciar mi nueva vida en España, llegue a españa el 12 de Octubre del 2017 junto a mi esposa, llenos de iluciones y ganas de salir adelante, a los tres meses consegui mi primer trabajo como dependiente de tienda en la cual me ofrecieron la posibilidad de aprender a reparar equipos eléctronicos, sin dudar acepte y aprendi todo lo que me enseñaron y con el tiempo reforce esos conocimientos y me especialice mas en el sector, sin embargo, no me llenaba del todo. Siempre me ha gustado realizar formatos en excel y hacer fórmulas, entonces por que no estudiar algo en lo que podre hacer de todo, por tal motivo decidi estudiar desarrollo web.
   
     El 20 de Agosto del 2020 nacio mi hermosa hija.
   
@@ -16,108 +17,119 @@ const information = [
   
     Dar lo mejor de mi para obtener los mejores resultados y sin ponerme limites para aprender lo que sea necesario en el trayecto.
     `,
-    personalObjectives: [
-      `Hacer crecer nuestro negocio y emprender más`,
-      `Crecer junto a mi familia y poder pasar mas tiempo juntos`,
-    ],
+      personalObjectives: [
+        `Hacer crecer nuestro negocio y emprender más`,
+        `Crecer junto a mi familia y poder pasar mas tiempo juntos`,
+      ],
 
-    jobsGoals: [
-      `Aprender cada día mas sobre el desarrollo de aplicaciones web`,
-      `Escalar y llegar a ser de mucho valor en la empresa que este`,
-      `Poder desarrollar aplicaciónes web de utilidad para mejores las empresas y negocios en general`,
-    ],
+      jobsGoals: [
+        `Aprender cada día mas sobre el desarrollo de aplicaciones web`,
+        `Escalar y llegar a ser de mucho valor en la empresa que este`,
+        `Poder desarrollar aplicaciónes web de utilidad para mejores las empresas y negocios en general`,
+      ],
 
-    whyMe: `Quizas mi experiencia actual no haga saltar tu interés, pero te aseguro que si me dan una oportunidad no te arrepentiras, por que hay algo que no te da la experiencia y son las ganas de aprender, la actitud para ello y el espíritu necesario para crecer, los conocimientos se construyen y da igual los que necesite seguro paso a paso los obtendré`,
+      whyMe: `Quizas mi experiencia actual no haga saltar tu interés, pero te aseguro que si me dan una oportunidad no te arrepentiras, por que hay algo que no te da la experiencia y son las ganas de aprender, la actitud para ello y el espíritu necesario para crecer, los conocimientos se construyen y da igual los que necesite seguro paso a paso los obtendré`,
 
-    photoURL: ``,
+      photoURL: ``,
 
-    callToAction: `Llámame - ¿Que es lo peor que puede pasar?`,
-  }),
-  (Education = {
-    name: `Education`,
-    educations: [
-      {
-        tittle: `DESARROLLADOR WEB FULL STACK`,
-        trainingCenter: `ADECCO Y RED.ES`,
-        year: `2022`,
-        timeContents: `230 Hours`,
-        contents: [
-          `Programación (HTML, CSS Y JAVASCRIPT)`,
-          `BBDD (MySQL)`,
-          `JAVA`,
-        ],
-      },
-      {
-        tittle: `FUNDAMENTOS DE JAVASCRIPT`,
-        trainingCenter: `PLATZI`,
-        year: `2021`,
-        timeContents: `230 Hours`,
-        contents: [
-          `Introducción a la programación`,
-          `Fundamentos de JavaScript`,
-        ],
-      },
-      {
-        tittle: `REPARACIÓN DE DISPOSITIVOS ELECTRÓNICOS`,
-        trainingCenter: `IRIPARO`,
-        year: `2018`,
-        timeContents: `80 Hours`,
-        contents: [
-          `Reparación de móviles`,
-          `Reparación de tablets`,
-          `Reparación de portatiles`,
-        ],
-      },
-      {
-        tittle: `INGENIERÍA ELECTRÓNICA     AUTOMATIZACIÓN Y CONTROL`,
-        trainingCenter: `URBE-VENEZUELA`,
-        year: `2017`,
-        timeContents: `FINALIZADO EN 2º CURSO`,
-      },
-      {
-        tittle: `BACHILLER EN CIENCIAS`,
-        trainingCenter: `URBE-VENEZUELA`,
-        year: `2010`,
-      },
-    ],
-  }),
-  (Experience = {
-    name: `Experiences`,
-    experiences: [
-      {
-        job: `PRÁCTICAS CURSO PROGRAMADOR WEB
+      callToAction: `Llámame - ¿Que es lo peor que puede pasar?`,
+    },
+  },
+  {
+    Education: {
+      name: `Education`,
+      educations: [
+        {
+          tittle: `DESARROLLADOR WEB FULL STACK`,
+          trainingCenter: `ADECCO Y RED.ES`,
+          year: `2022`,
+          timeContents: `230 Hours`,
+          contents: [
+            `Programación (HTML, CSS Y JAVASCRIPT)`,
+            `BBDD (MySQL)`,
+            `JAVA`,
+          ],
+        },
+        {
+          tittle: `FUNDAMENTOS DE JAVASCRIPT`,
+          trainingCenter: `PLATZI`,
+          year: `2021`,
+          timeContents: `230 Hours`,
+          contents: [
+            `Introducción a la programación`,
+            `Fundamentos de JavaScript`,
+          ],
+        },
+        {
+          tittle: `REPARACIÓN DE DISPOSITIVOS ELECTRÓNICOS`,
+          trainingCenter: `IRIPARO`,
+          year: `2018`,
+          timeContents: `80 Hours`,
+          contents: [
+            `Reparación de móviles`,
+            `Reparación de tablets`,
+            `Reparación de portatiles`,
+          ],
+        },
+        {
+          tittle: `INGENIERÍA ELECTRÓNICA     AUTOMATIZACIÓN Y CONTROL`,
+          trainingCenter: `URBE-VENEZUELA`,
+          year: `2017`,
+          timeContents: `FINALIZADO EN 2º CURSO`,
+        },
+        {
+          tittle: `BACHILLER EN CIENCIAS`,
+          trainingCenter: `URBE-VENEZUELA`,
+          year: `2010`,
+        },
+      ],
+    },
+  },
+  {
+    Experience: {
+      name: `Experiences`,
+      experiences: [
+        {
+          job: `PRÁCTICAS CURSO PROGRAMADOR WEB
         FULL STACK`,
-        company: `ADECCO Y RED.ES`,
-        timeWorking: `2022`,
-        functions: [
-          `Curriculum - HTML4`,
-          `Réplica de pagina de google - HTML5 / CSS3`,
-          `Sistema de gestión de cafetería - JAVA SWING`,
-          `Gestión de venta de entradas futbol - JAVA SERVLETS`,
-          `Sistema de quejas por distritos Madrid- JAVA SERVLETS
+          company: `ADECCO Y RED.ES`,
+          timeWorking: `2022`,
+          functions: [
+            `Curriculum - HTML4`,
+            `Réplica de pagina de google - HTML5 / CSS3`,
+            `Sistema de gestión de cafetería - JAVA SWING`,
+            `Gestión de venta de entradas futbol - JAVA SERVLETS`,
+            `Sistema de quejas por distritos Madrid- JAVA SERVLETS
           `,
-        ],
-      },
-      {
-        job: `TÉCNICO EN REPARACIÓN DE DISPOSITIVOS
+          ],
+        },
+        {
+          job: `TÉCNICO EN REPARACIÓN DE DISPOSITIVOS
         ELECTRÓNICOS`,
-        company: `IRIPARO / ANOVO`,
-        timeWorking: `2018-2022`,
-        functions: [
-          `Reparación de dispositivos con software (ODIN - SAMSUNG)`,
-          `Reparación de dispositivos Hardware (Multi-marca)`,
-          `Reparaciones de nivel 3 soldadura y micro-soldadura`,
-        ],
-      },
-      {
-        job: `SOPORTE TÉCNICO REDES`,
-        company: `SAETA INGENIERÍA `,
-        timeWorking: `2013-2017`,
-        functions: [
-          `Configuración de equipos de redes (CISCO).
+          company: `IRIPARO / ANOVO`,
+          timeWorking: `2018-2022`,
+          functions: [
+            `Reparación de dispositivos con software (ODIN - SAMSUNG)`,
+            `Reparación de dispositivos Hardware (Multi-marca)`,
+            `Reparaciones de nivel 3 soldadura y micro-soldadura`,
+          ],
+        },
+        {
+          job: `SOPORTE TÉCNICO REDES`,
+          company: `SAETA INGENIERÍA `,
+          timeWorking: `2013-2017`,
+          functions: [
+            `Configuración de equipos de redes (CISCO).
         `,
-        ],
-      },
-    ],
-  }),
+          ],
+        },
+      ],
+    },
+  },
 ];
+
+export default (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify(information));
+};
